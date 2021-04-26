40f31998d1c0f22c214b520b4c367b3a0a4eab3e
Usama Taj <usama.taj@hotmail.com>
2021-04-27 02:21:18 +0500

    adding customers table

--- src/Components/RouteComponents/Customers.js
+++ src/Components/RouteComponents/Customers.js
@@ -8,6 +8,12 @@ import TableRow from "@material-ui/core/TableRow";
 import TableContainer from "@material-ui/core/TableContainer";
 import makeStyles from "@material-ui/core/styles/makeStyles";
 import "bootstrap/dist/css/bootstrap.min.css";
+import customers from "../../Data/customers";
+
+import DeleteIcon from "@material-ui/icons/Delete";
+import EditIcon from "@material-ui/icons/Edit";
+import ViewComfyIcon from "@material-ui/icons/ViewComfy";
+import { IconButton } from "@material-ui/core";
 
 const useStyles = makeStyles({
   table: {
@@ -21,33 +27,63 @@ const useStyles = makeStyles({
 
 const Customers = () => {
   const classes = useStyles();
+  const cust = customers;
   return (
     <Paper className={classes.paper}>
       <TableContainer component={Paper}>
         <Table className={classes.table}>
           <TableHead>
-            <TableCell>
-              <b>#ID</b>
-            </TableCell>
-            <TableCell>
-              <b>First Name</b>
-            </TableCell>
-            <TableCell>
-              <b>Last Name</b>
-            </TableCell>
-            <TableCell>
-              <b>Email</b>
-            </TableCell>
-            <TableCell>
-              <b>Phone</b>
-            </TableCell>
-            <TableCell>
-              <b>Status</b>
-            </TableCell>
-            <TableCell>
-              <b>Action</b>
-            </TableCell>
+            <TableRow>
+              <TableCell align="right">
+                <b>#ID</b>
+              </TableCell>
+              <TableCell align="right">
+                <b>First Name</b>
+              </TableCell>
+              <TableCell align="right">
+                <b>Last Name</b>
+              </TableCell>
+              <TableCell align="right">
+                <b>Email</b>
+              </TableCell>
+              <TableCell align="right">
+                <b>Phone</b>
+              </TableCell>
+              <TableCell align="right">
+                <b>Status</b>
+              </TableCell>
+              <TableCell align="right">
+                <b>Action</b>
+              </TableCell>
+            </TableRow>
           </TableHead>
+          <TableBody>
+            {cust.map(
+              ({ id, first_name, last_name, email, phone, status }, index) => (
+                <TableRow key={index}>
+                  <TableCell component="th" scope="row">
+                    {id}
+                  </TableCell>
+                  <TableCell align="right">{first_name}</TableCell>
+                  <TableCell align="right">{last_name}</TableCell>
+                  <TableCell align="right">{email}</TableCell>
+                  <TableCell align="right">{phone}</TableCell>
+                  <TableCell align="right">{status}</TableCell>
+                  <TableCell align="right">
+                    <IconButton>
+                      <EditIcon fontSize="small" />
+                    </IconButton>
+                    <IconButton>
+                      <DeleteIcon fontSize="small" />
+                    </IconButton>
+                    <IconButton>
+                      <ViewComfyIcon fontSize="small" />
+                    </IconButton>
+                  </TableCell>
+                </TableRow>
+              )
+            )}
+          </TableBody>
         </Table>
       </TableContainer>
     </Paper>
